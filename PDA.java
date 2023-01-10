e2cb3fa7901ff33347df3e049c3aafd0cf4b1908
gabrielsalinas24 <113557530+gabrielsalinas24@users.noreply.github.com>
2023-01-10 14:19:17 -0800

    Update PDA.java

--- PDA.java
+++ PDA.java
@@ -19,35 +19,36 @@ public class PDA
         // We don't need to do anything in the constructor for
         // our program.
     }
-
-    /**
+        /**
      * This is the main event loop for our PDA program
      */
 
     public void runEventLoop() {
-        while (true) {
+        boolean shouldContinue = true;
+        while (shouldContinue){
             System.out.println("How old are you?");
+            System.out.println("Enter 0 to exit");
             try {
                 age = scanner.nextInt();
                 System.out.println(age);
-                LOWER_BOUND= (age/ 2) + 7;
-                int olderAge = (age- 7) * 2;
-                if (age < LOWER_BOUND && age > 0 || age <18 && age > 0) {
+                LOWER_BOUND= (int)Math.ceil((age/ 2.0) + 7);
+                int olderAge = (int)Math.ceil((age- 7) * 2);
+                if (age == 0) {
+                    shouldContinue = false;
+                } else if (age < LOWER_BOUND){
                     System.out.println(age+" is too young!!");
-                } else {
-                    if (age >= 18) {
-                        System.out.println(age+" good to go");
+                } else if (age >= 14) {
+                        System.out.println("youngest age " + LOWER_BOUND + " oldest age " + olderAge);
                     }
-                }
             } catch (InputMismatchException error) {
                 System.out.println("Please enter an integer");
             }
             if (age <= -1 || age <= 100 && age > 99) {
                 System.out.println("Please enter a proper age");
             }
-                    }
+        }
     }
-    
+
     /**
      * The main method instantiates and runs the program
      */
